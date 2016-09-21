0228a699770eed7170c1112d43db7632e7c36731
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2016-09-21 12:29:54 -0500

    Integrando buscador

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -63,11 +63,13 @@ $(document).on("scroll", function(event) {
   }
 	else{
   	barra_render = false;
-		if(ancho_act > 1165){
-			$("#search-lat").css("display","none");
+		if(ancho_act <= 1165){
+			$("#search-lat").removeAttr("style");
+			$("#forma-busqueda").css("display","none");
 		}
 		else{
-			$("#search-lat").removeAttr("style");
+			$("#search-lat").css("display","none");
+			$("#forma-busqueda").removeAttr("style");
 		}
 		$("#forma-busqueda").removeAttr("style");
 		$("#idioma-big-scroll").removeAttr("style");
