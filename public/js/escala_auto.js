e80714c062c032ddbce6afe6c3d3da57ddd42321
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-01-17 17:00:54 -0600

    Videos responsivos

--- public/js/escala_auto.js
+++ public/js/escala_auto.js
@@ -6,8 +6,17 @@ $(document).ready(function(){
 });
 
 window.onresize = function(){
-
+	reescalarFrames();
 	$(document).trigger("scroll");
+}
+
+function reescalarFrames(){
+	var frames = document.getElementsByTagName("iframe");
+	for(var i = 0; i < frames.length - 1; i++){
+		if(frames[i].clientWidth < 560){
+			frames[i].height = frames[i].clientWidth*315/560;
+		}
+	}
 }
 
 $(window).on("orientationchange",function(){
